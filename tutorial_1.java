dac0ec5d19b790ebb546793be523ad4ee21f7bec
DevCeylon <devinda.20222136@iit.ac.lk>
2023-08-01 00:39:49 +0530

    example 1 and 2 added

---
+++ tutorial_1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class tutorial_1 {
+    public static void main(String[] args){
+        /* Q1: Print your name and age*/
+        System.out.println("My name is Devinda rukshan jayasooriya");
+        System.out.println("My age is 21");
+
+        /*Write a program (use any name you like) and create a variable called runningTotal.
+        Assign 0 to variable “runningTotal”.Then, add numbers 5, 8, 2, 3 (one at a time) to“runningTotal”
+        variable.*/
+
+        int runningTotal = 0;
+        runningTotal = 5;
+        runningTotal = 8;
+        runningTotal = 2;
+        runningTotal = 3;
+        System.out.println(runningTotal);
+
+    }
+}
